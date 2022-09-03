01d1555e270c87b0c0787961434517cb3b60447f
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-09-03 23:53:39 +0530

    Refer Readme of this Repository for Output.!!
    Refer Readme of this Repository for Output.!!
    For output of the above code refer to Readme of this repository, file name is saved accordingly to patterns numberings..#include <stdio.h>

---
+++ 6.c
@@ -0,0 +1,18 @@
+int main()
+{
+    int i,a;
+    i = 1;
+    while(i<=5)
+    {
+        a=i;
+        while(a <= 5)
+        {
+            printf("%d",a);
+            a=a+1;
+        }
+         printf("\n");
+         i=i+1;
+    }
+
+    return 0;
+}
