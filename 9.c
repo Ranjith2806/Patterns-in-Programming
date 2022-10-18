7d860b4b6a535bdc71f7fcd4629693c54db3c426
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-10-18 23:00:25 +0530

    Refer Readme of this repo for output

---
+++ 9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n, x, y;
+printf("Enter number of rows to show character pattern: ");
+scanf("%d",&n);
+for(x = 1; x <= n; x++)
+{
+for(y = 1; y <= x; y++)
+{
+printf("%c",'A'-1 + x);
+}
+printf("\n");
+}
+return 0;
+}
